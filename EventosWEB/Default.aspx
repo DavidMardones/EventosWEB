5f1a7d8fcad16c5d5258dc95bb358c3a7644442a
David Mardones <david.mardones04@inacapmail.cl>
2023-07-14 09:11:19 -0400

    Agregar archivos de proyecto.

---
+++ EventosWEB/Default.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EventosWEB.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
---
+++ EventosWEB/MostrarAsistente.aspx.cs
@@ -0,0 +1,29 @@
+﻿using EventosModel;
+using EventosModel.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventosWEB
+{
+    public partial class MostrarAsistente : System.Web.UI.Page
+    {
+        private IAsistentesDAL asistentesDAL = new AsistentesDALDB();
+
+        private void cargaGrilla(List<Asistente> asistentes)
+        {
+            this.grillaAsistente.DataSource = asistentes;
+            this.grillaAsistente.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                this.cargaGrilla(this.asistentesDAL.ObtenerAsistentes());
+            }
+        }
+    }
+}
